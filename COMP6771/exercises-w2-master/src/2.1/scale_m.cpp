605e4e7b04555e3e7e3e3957b4d65ec460e6de75
tuanminh25 <tuanminh1256@gmail.com>
2025-06-09 15:21:46 +1000

    add note

--- COMP6771/exercises-w2-master/src/2.1/scale_m.cpp
+++ COMP6771/exercises-w2-master/src/2.1/scale_m.cpp
@@ -1,6 +1,36 @@
 #include "scale.h"
 
-// what is static and dynamic cast?
+/*
+static_cast: Fast but unsafe (no type check).
+
+dynamic_cast: Safe but slower (uses RTTI for type check).
+
+what is static and dynamic cast?
+
+static_cast -> when sure cast is safe at compile time (speed over robust)
+
+    + Compile-time cast.
+
+    + Used when the conversion is known to be safe (e.g., base to derived or vice versa, primitive types).
+
+    + No runtime check, so invalid casts can lead to undefined behavior.
+
+    + Faster, but less safe.
+
+dynamic_cast -> not sure type need safe check (robust over speed)
+
+    + Runtime cast, used with polymorphic types (must have at least one virtual function).
+
+    + Safe for casting down the inheritance hierarchy.
+
+    + Performs a runtime type check.
+
+        Returns a valid pointer or nullptr for invalid pointer casts.
+
+        Throws std::bad_cast for invalid references.
+
+
+*/
 
 
 // use static_cast to change from vec int to vec double, 
