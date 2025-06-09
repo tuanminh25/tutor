335a6bd2f2b743508632a1c107c490c07b05edb4
tuanminh25 <tuanminh1256@gmail.com>
2025-06-09 23:46:51 +1000

    clang format 2.3

--- COMP6771/exercises-w2-master/src/2.3/permutation.cpp
+++ COMP6771/exercises-w2-master/src/2.3/permutation.cpp
@@ -1,6 +1,6 @@
 #include "permutation.h"
 
-auto is_permutation(const std::string &x, const std::string &y) -> bool {
-    // remove this line when properly implemented
-    return static_cast<bool>(x.size()) && static_cast<bool>(y.size());
+auto is_permutation(const std::string& x, const std::string& y) -> bool {
+	// remove this line when properly implemented
+	return static_cast<bool>(x.size()) && static_cast<bool>(y.size());
 }
--- COMP6771/exercises-w2-master/src/2.3/permutation.h
+++ COMP6771/exercises-w2-master/src/2.3/permutation.h
@@ -7,6 +7,6 @@
  * Determines if the string `x` is a permutation of the string `y`.
  * It is possible for one or either of these strings to be the empty string.
  */
-auto is_permutation(const std::string &x, const std::string &y) -> bool;
+auto is_permutation(const std::string& x, const std::string& y) -> bool;
 
 #endif // COMP6771_PERMUTATION_H
