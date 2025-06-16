d22e30fa945f4dabd929279511607c001cf8af69
Mason Truong <z5381092@ad.unsw.edu.au>
2025-06-16 21:56:35 +1000

    3.5

--- COMP6771/exercises-w3-master/src/3.5/m.h
+++ COMP6771/exercises-w3-master/src/3.5/m.h
@@ -10,6 +10,11 @@ struct celestial_body {
 };
 
 // Hint: type aliases in modern C++ also use the "using" directive...
+/*
+Namespace: groups code to avoid name conflicts, accessed via :: (e.g. std::cout).
+
+using: creates shortcuts for namespaces or types (e.g. using std::cout;, using int_vector = std::vector<int>;).
+*/
 namespace comp6771 {
     using vector = std::vector<int>;
     using planet = celestial_body;
